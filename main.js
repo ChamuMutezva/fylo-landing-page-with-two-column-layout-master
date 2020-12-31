c3bbb346c0f4ef136fb39f712d2371773999c44a
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-12-31 22:47:35 +0200

    js validation final

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 const forms = Array.from(document.querySelectorAll("form"));
-console.log(forms)
+
+const heroemail = document.querySelector(".hero__email")
+const footeremail = document.querySelector(".footer__email")
+
+const btnHero = document.querySelector(".btnSubmit__hero")
+const btnFooter = document.querySelector(".footer__btnSubmit")
+
+const footerSpanErr = document.querySelector(".errorMsg__footer")
+const spanErr = document.querySelector(".errorMsg")
+
+
+
+heroemail.addEventListener("invalid", () => {
+    spanErr.classList.add("showError")
+    btnHero.classList.add("btnSpacer")
+})
+
+heroemail.addEventListener("input", () => {   
+    spanErr.classList.remove("showError")
+    btnHero.classList.remove("btnSpacer") 
+})
+
+
+footeremail.addEventListener("invalid", () => {
+    footerSpanErr.classList.add("showError")
+    btnFooter.classList.add("btnSpacer")
+})
+
+footeremail.addEventListener("input", () => {
+    footerSpanErr.classList.remove("showError")
+    btnFooter.classList.remove("btnSpacer")
+})
+
 forms.forEach(form => {
     form.addEventListener("submit", (evt) => {
         //console.log("clicked")
